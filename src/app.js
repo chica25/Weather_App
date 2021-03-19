443848f7ea7b01371b34b3cf163055bacb8a0455
chica25 <patricia@patriciaco.com>
2021-03-19 01:33:47 -0400

    Setup Api key and and url

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 const key = "GTCAYBb3uEuh2F18AG7qHxMnkiJwdFgs";
+
+const getCity  = async (city) => {
+    const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const queryParameter = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch( baseUrl + queryParameter)
+    const data = await response.json();
+
+    return data[0];
+};
+
+getCity('london').then(data => console.log(data))
+.catch(err => console.log(err))
