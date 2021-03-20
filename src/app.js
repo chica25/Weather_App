8cb3c3df55c92675bd5a3006cc823a0940cd421c
chica25 <patricia@patriciaco.com>
2021-03-19 20:29:18 -0400

    Executed fetch request

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
 const key = "GTCAYBb3uEuh2F18AG7qHxMnkiJwdFgs";
+// get weather info
 
-const getCity  = async (city) => {
-    const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-    const queryParameter = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch( baseUrl + queryParameter)
+const getWeather = async (id) => {
+    const baseUrl = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const queryParameter = `${id}?apikey=${key}`;
+   
+const response = await fetch(baseUrl + queryParameter);
     const data = await response.json();
 
     return data[0];
 };
 
-getCity('london').then(data => console.log(data))
-.catch(err => console.log(err))
+const getCity = async (city) => {
+
+    const baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const queryParameter = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(baseUrl + queryParameter);
+    const data = await response.json();
+    
+    return data[0];
+};
+
+getCity('new york').then(data => {
+    return getWeather(data.Key)
+}).then(data => {
+    console.log(data);
+}).catch(err => console.log(err))
+
+
+// getWeather("349727");
