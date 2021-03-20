8e51394bcdfeda7b94f8d22acfa253781ffb2657
chica25 <patricia@patriciaco.com>
2021-03-19 21:14:24 -0400

    Edit html and insert properties

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 const cityForm = document.querySelector('.change-location');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    const citySpecs = data.citySpecs;
+    const weather = data.weather
+
+    details.innerHTML = `
+        <h5 class="my-3">${citySpecs.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4">
+            <span>${weather.Temperature.Imperial.Value}</span>
+            <span>&deg;C</span>
+        </div>
+     `;
+};
+
 
 const updateCity = async (city) => {
+
     const citySpecs = await getCity(city);
     const weather = await getWeather(citySpecs.Key);
 
@@ -16,6 +35,6 @@ cityForm.addEventListener('submit', e => {
 
     //update the UI with new city
     updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 })
